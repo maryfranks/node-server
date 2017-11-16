7731794a66724bc773db6f0fd481b6a24b6d021f
maryfranks <maryfranks@gmail.com>
2017-11-16 09:19:17 -0500

    establish basic file server functionality

--- fileserver.js
+++ fileserver.js
@@ -1,7 +1,21 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
+const port = 3000
 
 http.createServer(function(request, response) {
 
-}).listen(3000);
+  console.log(`${request.method} request for ${request.url}`);
+
+  if (request.url === "/") {
+    response.writeHead(200, {"Content-Type": "text/plain"})
+    response.end("Hello World!")
+
+  } else {
+    response.writeHead(404, {"Content-Type": "text/plain"})
+    response.end("404: File not Found")
+  }
+
+}).listen(port);
+
+console.log(`Server listening on ${port}`)
